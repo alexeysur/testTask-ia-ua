f4585822b02c2b10afebc80e283bf5bc73bdff0b
Alexey Sur <alexeysur@gmail.com>
2021-08-19 00:18:12 +0300

    feat: return project

---
+++ BankingApp/BankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Alexey on 19.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
